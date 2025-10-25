3285e4909f4e94f2fe78f8d509a896ffddc66d9e
谢m <2991265228@qq.com>
2025-10-26 05:34:30 +0800

    新增商品详情页面，拦截器白名单接口

--- src/main/java/com/itheima/simpleShoppingMallDemo/Config/WebMvcConfig.java
+++ src/main/java/com/itheima/simpleShoppingMallDemo/Config/WebMvcConfig.java
@@ -33,8 +33,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "/home/catlist",
                         "/home/prolist",
                         "/home/bycategory",
-                        "product/getComments",
-                        "product/show"
+                        "/product/getComments",
+                        "/product/show"
                 );
     }
 }
